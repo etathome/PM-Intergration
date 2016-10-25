f3e3e66941f9ea631c442d37414f21938b9fc39d
Feng Gao <feng.gao@oneadvacned.com>
2016-10-25 08:29:54 +0100

    changes in 3nd Branch

--- ProMonitorIntegrationTest/ApiEndPoints.cs
+++ ProMonitorIntegrationTest/ApiEndPoints.cs
@@ -6,6 +6,6 @@
         //public const string Qualification = "Partners/Qualifications";
         public const string LearnerProgress = "Learner/GetLearnerProgress";
         public const string Qualification = "Course/GetQualifications";
-        public const string test = "test5";
+        public const string strTest = "3nd";
     }
 }
